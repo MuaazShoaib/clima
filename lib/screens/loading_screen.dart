50b3bc73a0b58d6e81bec7e9c60cc131e51e13e5
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-07-19 11:19:37 +0500

    144. Async/Await Challenge - Completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,37 +14,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      // somethingThatExpectsLessThan10(12);
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (exception) {
-      print(exception);
-    }
-  }
-
-  void somethingThatExpectsLessThan10(int n) {
-    if (n > 10) {
-      throw 'n is greater than 10, n should always be less than 10.';
-    }
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    // String myMargin = '15';
-    String myMargin = 'abc';
-
-    double myMarginAsADouble = 0.0;
-    try {
-      myMarginAsADouble = double.parse(myMargin);
-    } catch (exception) {
-      print(exception);
-      myMarginAsADouble = 30.0;
-    }
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.all(myMarginAsADouble),
         color: Colors.red,
       ),
     );
