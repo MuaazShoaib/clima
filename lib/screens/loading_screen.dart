e52b04d13c9bccd9325ccd0e1b4cc284b6ec9db0
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-07-20 23:50:26 +0500

    148. Getting Actual Weather Data from the OpenWeatherMap API - Completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
-
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+const apiKey = 'a43896ce1a975826c5dbee925e9418f9';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,70 +10,54 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     super.initState();
-    // getLocation();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
     // getData();
   }
 
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    // print(location.latitude);
-    // print(location.longitude);
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
-      Uri.parse(
-          // Islamabad Location
-          'https://api.openweathermap.org/data/2.5/weather?lat=33.684422&lon=73.047882&appid=a43896ce1a975826c5dbee925e9418f9'),
-
-      //   Uri.parse(
-      //       'https://api.openweathermap.org/data/2.5/heather?lat=35&lon=139&appid=a43896ce1a975826c5dbee925e9418f9'),
-    );
-
-    // print(response.body);
-    // print(response.statusCode);
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-      // print(data);
-
-      // var longitute = jsonDecode(data)['coord']['lon'];
-      // print(longitute);
-      //
-      // var weatherDescription = jsonDecode(data)['weather'][0]['description'];
-      // print(weatherDescription);
-      //
-      // // weather[0].description
-
-      var decodedData = jsonDecode(data);
-
-      // var temperature = jsonDecode(data)['main']['temp'];
-      // var cityName = jsonDecode(data)['name'];
-      // var condition = jsonDecode(data)['weather'][0]['id'];
-
-      // var temperature = decodedData['main']['temp'];
-      // var cityName = decodedData['name'];
-      // var condition = decodedData['weather'][0]['id'];
-
-      double temperature = decodedData['main']['temp'];
-      String cityName = decodedData['name'];
-      int condition = decodedData['weather'][0]['id'];
-
-      print(temperature);
-      print(cityName);
-      print(condition);
-    } else {
-      print(response.statusCode);
-    }
-  }
+  // void getData() async {
+  // http.Response response = await http.get(
+  //   Uri.parse(
+  //       'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'),
+  // );
+  //
+  // if (response.statusCode == 200) {
+  //   String data = response.body;
+  //
+  //   var decodedData = jsonDecode(data);
+  //
+  //   double temperature = decodedData['main']['temp'];
+  //   String cityName = decodedData['name'];
+  //   int condition = decodedData['weather'][0]['id'];
+  //
+  //   print(temperature);
+  //   print(cityName);
+  //   print(condition);
+  // } else {
+  //   print(response.statusCode);
+  // }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Container(
         color: Colors.red,
