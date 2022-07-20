cd6e587bc58ae17d3a9d902e413c78bf3cec55fb
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-07-21 02:31:29 +0500

    152. Refactoring the Location Methods - Completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,9 @@
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-const apiKey = 'a43896ce1a975826c5dbee925e9418f9';
+// const apiKey = 'a43896ce1a975826c5dbee925e9418f9';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,14 +21,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
+    // Location location = Location();
+    // await location.getCurrentLocation();
+    //
+    // NetworkHelper networkHelper = NetworkHelper(
+    //     'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    //
+    // var weatherData = await networkHelper.getData();
 
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
     Navigator.push(
       this.context,
       MaterialPageRoute(builder: (context) {
