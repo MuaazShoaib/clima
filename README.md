cc7728945f982990435b0912d7520afcf9d89b6a
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-07-20 15:42:57 +0500

    146. Networking in Flutter Apps with the HTTP Package - Completed

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Section 13: Clima - Powering Your Flutter App with Live Weather Web Data
 
-Course Notes: https://github.com/MuaazShoaib/mobile-app-develoment-skills-log/tree/main/Notes
+Course Notes: https://github.com/MuaazShoaib/mobile-app-develoment-skills-log/tree/main/learning-flutter/flutter-bootcamp-2021/notes
 
 Course Link: https://www.udemy.com/course/flutter-bootcamp-with-dart/
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    // getData();
   }
 
   void getLocation() async {
@@ -20,8 +22,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a43896ce1a975826c5dbee925e9418f9'),
+
+      //   Uri.parse(
+      //       'https://api.openweathermap.org/data/2.5/heather?lat=35&lon=139&appid=a43896ce1a975826c5dbee925e9418f9'),
+    );
+
+    // print(response.body);
+    // print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Container(
         color: Colors.red,
